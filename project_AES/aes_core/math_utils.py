8e74ea0fcd184048b5c3514330614aaec4e26b19
shrimp2845-tw <goldencheng15@gmail.com>
2026-05-09 23:20:20 +0800

    feat(math_utils): add xtime128 and gfm128

--- project_AES/aes_core/math_utils.py
+++ project_AES/aes_core/math_utils.py
@@ -7,6 +7,14 @@ def xtime(a: int) -> int:
         a ^= 0x11B
     return a & 0xFF
 
+def xtime128(a: int) -> int:
+    """perform a × 2 within GF(2¹²⁸)"""
+    msb = a & (1 << 127)
+    a <<= 1
+    if msb:
+        a ^= 0xE1000000000000000000000000000000
+    return a & ((1 << 128) - 1)
+
 @cache
 def gfm(a: int, b: int) -> int:
     """preform a × b within GF(2⁸)"""
@@ -15,5 +23,15 @@ def gfm(a: int, b: int) -> int:
         if b & 1:
             result ^= a
         a = xtime(a)
+        b >>= 1
+    return result
+
+def gfm128(a: int, b: int) -> int:
+    """preform a × b within GF(2¹²⁸)"""
+    result = 0
+    for i in range(8):
+        if b & 1:
+            result ^= a
+        a = xtime128(a)
         b >>= 1
     return result
